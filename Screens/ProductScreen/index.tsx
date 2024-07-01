2e15ba80334f9eb8a42b3922d331536a173c96ea
sammyjay076 <boakyesamuel076@gmail.com>
2024-07-01 20:04:45 +0000

    implemented the shop logic

--- Screens/ProductScreen/index.tsx
+++ Screens/ProductScreen/index.tsx
@@ -4,18 +4,30 @@ import products from "../../data/ProductData";
 import ProductCard from "../../components/ProductCard";
 
 type Props = {};
+interface IProductScreenProps {
+  addItemCheckout: (item: any) => void;
+  navigation: any;
+}
 
-const ProductScreen = (props: Props) => {
+const ProductScreen: React.FC<IProductScreenProps> = ({
+  addItemCheckout,
+  navigation,
+}) => {
   return (
     <View>
       <Text>ProductScreen</Text>
       <FlatList
+        numColumns={2}
         data={products}
         renderItem={({ item }) => (
           <ProductCard
             name={item.name}
             price={item.price}
             description={item.description}
+            onpress={() => {
+              console.log("Product added to cart.", item);
+              addItemCheckout(item);
+            }}
           />
         )}
       />
